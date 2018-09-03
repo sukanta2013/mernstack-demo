b164a6fcd304b0f2236bd9fa5b7cf07ae717ddf2
Sukanta Bhowmik <sukantabhowmik007@gmail.com>
2018-09-03 10:11:59 +0530

    added model called User for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
+
+//Load user model
+
+const User = require("../../models/User")
+//@route    GET api/users/test
+//@desc     Tests users route
+//@access   Public
 router.get("/test", (req, res) => {
   res.json({ msg: "users works" });
 });
+
+//@route    GET api/users/register
+//@desc     Tests users register
+//@access   Public
+router.post("/register", (req, res)=>{
+  User.findone({email: req.body.email})
+.then(user=>{
+  if(user){
+    return res.status(400).json({email:"email already exists"})
+  }else{
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      avatar,
+      password: req.body.password
+    })
+  }
+})
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-
+//Body parser midddleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 //DB config
 const db = require("./config/keys").mongoURI;
 //connecting to database
